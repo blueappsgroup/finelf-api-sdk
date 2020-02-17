7cca7708c530c110563830cf3f9ae78be2d4a023
Adrian <adrian.r@blueapps.pl>
2020-02-17 14:13:38 +0100

    add ksort to LenderDTO->offers

--- example/index.php
+++ example/index.php
@@ -7,3 +7,5 @@ use Finelf\Api;
 $api = Api::getInstance('USERNAME', 'PASSWORD', 'CLIENT_ID', 'CLIENT_SECRET', 'API_URL');
 $ranking = $api->ranking->get(2);
 //$lender = $api->lender->get(2);
+
+offer->get('')
