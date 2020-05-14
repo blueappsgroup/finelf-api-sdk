e3316e995d9795757615d684bc5d61c32cc040bb
Adrian <adrian.r@blueapps.pl>
2020-05-14 11:06:29 +0200

    retun one value if from and to value are the same in dateTimeRangeValueCalculate method

--- src/DTO/ParameterDTO.php
+++ src/DTO/ParameterDTO.php
@@ -84,6 +84,10 @@ class ParameterDTO extends BaseDTO {
             $fromValue = $from / $days;
             $toValue   = $to / $days;
 
+            if($fromValue == $toValue) {
+                return $fromValue . ' ' . self::DATE_TIME_SETTINGS['suffixes'][ $unit ];
+            }
+
             return $fromValue . '-' . $toValue . ' ' . self::DATE_TIME_RANGE_SETTINGS['suffixes'][ $unit ];
         }
 
