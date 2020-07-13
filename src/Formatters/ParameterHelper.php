6561e0daa8bc40688fb141decc3c356745300316
Mateusz Solecki <solmat@gmail.com>
2020-07-13 13:48:14 +0200

    add translations and update ParameterHelper

--- src/Formatters/ParameterHelper.php
+++ src/Formatters/ParameterHelper.php
@@ -13,128 +13,117 @@ use function str_replace;
 use function strpos;
 
 class ParamFormatter {
-    const SPECIAL_TYPES
-        = [
-            1 => 'formatNumberValue',
-            3 => 'formatRangeValue',
-            4 => 'formatBooleanValue',
-            5 => 'formatJSONValue',
-            6 => 'formatDateTimeRangeValue',
-        ];
-    const DATE_TIME_RANGE_SETTINGS
-        = [
-            'days'     => [
-                365,
-                30,
-                7,
-                1,
-            ],
-            'suffixes' => [
-                ['rok', 'lata', 'lat'],
-                ['miesiąc', 'miesiące', 'miesięcy'],
-                ['tydzień', 'tygodnie', 'tygodni'],
-                ['dzień', 'dni', 'dni'],
-            ],
-        ];
+	const SPECIAL_TYPES
+		= [
+			1 => 'formatNumberValue',
+			3 => 'formatRangeValue',
+			4 => 'formatBooleanValue',
+			5 => 'formatJSONValue',
+			6 => 'formatDateTimeRangeValue',
+		];
+	const DATE_TIME_RANGE_DAYS_OPTIONS = [ 365, 30, 7, 1, ];
 
-    public static function formatValue(string $prefix, string $suffix, string $value, int $type)
-    {
-        $returnedValue = $value;
+	public static function formatValue( string $prefix, string $suffix, string $value, int $type ) {
+		$returnedValue = $value;
 
-        if (isset(self::SPECIAL_TYPES[$type])) {
-            $returnedValue = self::{self::SPECIAL_TYPES[$type]}($value);
-        }
+		if ( isset( self::SPECIAL_TYPES[ $type ] ) ) {
+			$returnedValue = self::{self::SPECIAL_TYPES[ $type ]}( $value );
+		}
 
-        if (is_array($returnedValue)) {
-            return $returnedValue;
-        }
+		if ( is_array( $returnedValue ) ) {
+			return $returnedValue;
+		}
 
-        if ( ! empty($prefix)) {
-            $prefix .= ' ';
-        }
+		if ( ! empty( $prefix ) ) {
+			$prefix .= ' ';
+		}
 
-        if ( ! empty($suffix)) {
-            $returnedValue .= ' ';
-        }
+		if ( ! empty( $suffix ) ) {
+			$returnedValue .= ' ';
+		}
 
-        return $prefix.$returnedValue.$suffix;
-    }
+		return $prefix . $returnedValue . $suffix;
+	}
 
-    public static function formatRangeValue(string $value): string
-    {
-        $values = explode('-', $value);
-        $from   = array_key_exists(0, $values) ? self::formatNumberValue($values[0]) : 0;
-        $to     = array_key_exists(1, $values) ? self::formatNumberValue($values[1]) : 0;
+	public static function formatRangeValue( string $value ): string {
+		$values = explode( '-', $value );
+		$from   = array_key_exists( 0, $values ) ? self::formatNumberValue( $values[0] ) : 0;
+		$to     = array_key_exists( 1, $values ) ? self::formatNumberValue( $values[1] ) : 0;
 
-        return $from.' - '.$to;
-    }
+		return $from . ' - ' . $to;
+	}
 
-    public static function formatNumberValue(string $value): string
-    {
-        if (strpos($value, ',') !== false || strpos($value, '.') !== false) {
-            $value = (float)str_replace(',', '.', $value);
+	public static function formatNumberValue( string $value ): string {
+		if ( strpos( $value, ',' ) !== false || strpos( $value, '.' ) !== false ) {
+			$value = (float) str_replace( ',', '.', $value );
 
-            return number_format($value, 2, ',', ' ');
-        }
+			return number_format( $value, 2, ',', ' ' );
+		}
 
-        return number_format((float)$value, 0, ',', ' ');
-    }
+		return number_format( (float) $value, 0, ',', ' ' );
+	}
 
-    public static function formatBooleanValue(string $value): string
-    {
-        return $value === '1' ? 'Tak' : 'Nie';
-    }
+	public static function formatBooleanValue( string $value ): string {
+		return $value === '1' ? __( 'Tak', 'finelf-ranking' ) : __( 'Nie', 'finelf-ranking' );
+	}
 
-    public static function formatJSONValue(string $value): array
-    {
-        return json_decode(rawurldecode($value));
-    }
+	public static function formatJSONValue( string $value ): array {
+		return json_decode( rawurldecode( $value ) );
+	}
 
-    public static function formatDateTimeRangeValue(string $value): string
-    {
-        $values = explode('-', $value);
-        $from   = array_key_exists(0, $values) ? intval($values[0]) : 0;
-        $to     = array_key_exists(1, $values) ? intval($values[1]) : 0;
+	public static function formatDateTimeRangeValue( string $value ): string {
+		$values = explode( '-', $value );
+		$from   = array_key_exists( 0, $values ) ? intval( $values[0] ) : 0;
+		$to     = array_key_exists( 1, $values ) ? intval( $values[1] ) : 0;
 
-        if ($from && $to) {
-            return self::dateTimeRangeValueCalculate($from, $to);
-        }
+		if ( $from && $to ) {
+			return self::dateTimeRangeValueCalculate( $from, $to );
+		}
 
-        return '';
-    }
+		return '';
+	}
 
-    public static function dateTimeRangeValueCalculate(int $from, int $to, int $unit = 0): string
-    {
-        $days = self::DATE_TIME_RANGE_SETTINGS['days'][$unit];
+	public static function dateTimeRangeValueCalculate( int $from, int $to, int $unit = 0 ): string {
+		$days = self::DATE_TIME_RANGE_DAYS_OPTIONS[ $unit ];
 
-        if (($from % $days) == 0 && ($to % $days) == 0) {
-            $fromValue = $from / $days;
-            $toValue   = $to / $days;
+		if ( ( $from % $days ) == 0 && ( $to % $days ) == 0 ) {
+			$fromValue = $from / $days;
+			$toValue   = $to / $days;
 
-            if ($fromValue === $toValue) {
-                return self::formatSingleValueSuffix($fromValue, $unit);
-            }
+			if ( $fromValue === $toValue ) {
+				return self::formatSingleValueSuffix( $fromValue, $unit );
+			}
 
-            return $fromValue.' - '.$toValue.' '.self::DATE_TIME_RANGE_SETTINGS['suffixes'][$unit][2];
-        }
+			return $fromValue . ' - ' . $toValue . ' ' . self::getDateTimeRangeSuffix( $unit, 2 );
+		}
 
-        if ($unit > (count(self::DATE_TIME_RANGE_SETTINGS['days']) - 1)) {
-            return '';
-        }
+		if ( $unit > ( count( self::DATE_TIME_RANGE_DAYS_OPTIONS ) - 1 ) ) {
+			return '';
+		}
 
-        return self::dateTimeRangeValueCalculate($from, $to, $unit + 1);
-    }
+		return self::dateTimeRangeValueCalculate( $from, $to, $unit + 1 );
+	}
 
-    public static function formatSingleValueSuffix($value, $unit)
-    {
-        if ($value === 1) {
-            return $value.' '.self::DATE_TIME_RANGE_SETTINGS['suffixes'][$unit][0];
-        }
+	public static function formatSingleValueSuffix( $value, $unit ) {
+		if ( $value === 1 ) {
+			return $value . ' ' . self::getDateTimeRangeSuffix( $unit, 0 );
+		}
 
-        if ($value % 10 > 1 && $value % 10 < 5 && ! ($value % 100 >= 10 && $value % 100 <= 21)) {
-            return $value.' '.self::DATE_TIME_RANGE_SETTINGS['suffixes'][$unit][1];
-        }
+		if ( $value % 10 > 1 && $value % 10 < 5 && ! ( $value % 100 >= 10 && $value % 100 <= 21 ) ) {
+			return $value . ' ' . self::getDateTimeRangeSuffix( $unit, 1 );
+		}
 
-        return $value.' '.self::DATE_TIME_RANGE_SETTINGS['suffixes'][$unit][2];
-    }
+		return $value . ' ' . self::getDateTimeRangeSuffix( $unit, 2 );
+	}
+
+	public static function getDateTimeRangeSuffix( int $unit, int $element ): string {
+		$suffixes = [
+			[ __( 'rok', 'finelf-ranking' ), __( 'lata', 'finelf-ranking' ), __( 'lat', 'finelf-ranking' ) ],
+			[ __( 'miesiąc', 'finelf-ranking' ), __( 'miesiące', 'finelf-ranking' ), __( 'miesięcy', 'finelf-ranking' ) ],
+			[ __( 'tydzień', 'finelf-ranking' ), __( 'tygodnie', 'finelf-ranking' ), __( 'tygodni', 'finelf-ranking' ) ],
+			[ __( 'dzień', 'finelf-ranking' ), __( 'dni', 'finelf-ranking' ), __( 'dni', 'finelf-ranking' ) ],
+		];
+
+		return $suffixes[ $unit ][ $element ];
+	}
 }
