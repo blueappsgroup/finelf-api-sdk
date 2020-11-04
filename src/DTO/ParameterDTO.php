e8ef421b6640894a1ec65b3ca531334fdb549ece
Mateusz Solecki <solmat@gmail.com>
2020-11-04 11:07:31 +0100

    add rewValue to params

--- src/DTO/ParameterDTO.php
+++ src/DTO/ParameterDTO.php
@@ -5,18 +5,22 @@ namespace Finelf_Api_Sdk\DTO;
 use Finelf_Api_Sdk\Formatters\ParamFormatter;
 
 class ParameterDTO extends BaseDTO {
-    public $name;
-    public $type;
-    public $value;
-    public $slug;
-    public $description;
+	public $name;
+	public $type;
+	public $value;
+	public $slug;
+	public $description;
+	public $rawValue = '';
 
-    protected function parameter($parameter) {
-        $prefix      = $parameter->prefix === null ? '' : $parameter->prefix;
-        $suffix      = $parameter->suffix === null ? '' : $parameter->suffix;
-        $this->name  = $parameter->name;
-        $this->type  = $parameter->type;
-        $this->slug  = $parameter->slug;
-        $this->value = ParamFormatter::formatValue($prefix, $suffix, $this->value, $parameter->type);
-    }
+	protected function parameter( $parameter ) {
+		$prefix         = $parameter->prefix === null ? '' : $parameter->prefix;
+		$suffix         = $parameter->suffix === null ? '' : $parameter->suffix;
+		$this->name     = $parameter->name;
+		$this->type     = $parameter->type;
+		$this->prefix   = $prefix;
+		$this->suffix   = $suffix;
+		$this->rawValue = $this->value;
+		$this->slug     = $parameter->slug;
+		$this->value    = ParamFormatter::formatValue( $prefix, $suffix, $this->value, $parameter->type );
+	}
 }
