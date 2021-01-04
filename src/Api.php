ef7fe222a1560caec7810f4701c8c5b9700f185b
Mateusz Solecki <solmat@gmail.com>
2021-01-04 12:17:25 +0100

    revert

--- src/Api.php
+++ src/Api.php
@@ -5,53 +5,42 @@ namespace Finelf_Api_Sdk;
 use function class_exists;
 use function ucfirst;
 
-class Api
-{
-    private static $instance;
-    private $client;
+class Api {
+	private static $instance;
+	private $client;
 
-    private function __construct(string $username, string $password, string $apiURL)
-    {
-        $apiClient    = new ApiClient(
-            [
-                'username' => $username,
-                'password' => $password,
-                'uri'      => $apiURL,
-            ]
-        );
-        $this->client = $apiClient->createApiClient();
-    }
+	public function __construct( string $username, string $password, string $clientID, string $clientSecret, string $apiURL ) {
+		$apiClient    = new ApiClient(
+			[
+				'username'     => $username,
+				'password'     => $password,
+				'clientID'     => $clientID,
+				'clientSecret' => $clientSecret,
+				'uri'          => $apiURL,
+			]
+		);
+		$this->client = $apiClient->createApiClient();
+	}
 
-    public static function getInstance(string $username, string $password, string $apiURL): Api
-    {
-        $can_connect = ! empty($username) && ! empty($password) && ! empty($apiURL);
+	public static function getInstance( string $username, string $password, string $clientID, string $clientSecret, string $apiURL ): Api {
+		$can_connect = ! empty( $username ) && ! empty( $password ) && ! empty( $clientID ) && ! empty( $clientSecret ) && ! empty( $apiURL );
 
-        if ($can_connect && self::$instance === null) {
-            self::$instance = new Api($username, $password, $apiURL);
-        }
+		if ( $can_connect && self::$instance === null ) {
+			self::$instance = new Api( $username, $password, $clientID, $clientSecret, $apiURL );
+		}
 
-        return self::$instance;
-    }
+		return self::$instance;
+	}
 
-    public function __wakeup()
-    {
-        throw new \Exception('Singleton can not be unserialized');
-    }
+	public function __get( $name ) {
+		$class = 'Finelf_Api_Sdk\Modules\\' . ucfirst( $name ) . 'Module';
 
-    public function __get($name)
-    {
-        $class = 'Finelf_Api_Sdk\Modules\\'.ucfirst($name).'Module';
+		if ( class_exists( $class ) ) {
+			if ( ! isset( $this->$name ) ) {
+				$this->$name = new $class( $this->client );
+			}
 
-        if (class_exists($class)) {
-            if ( ! isset($this->$name)) {
-                $this->$name = new $class($this->client);
-            }
-
-            return $this->$name;
-        }
-    }
-
-    private function __clone()
-    {
-    }
+			return $this->$name;
+		}
+	}
 }
