6f88528de7e32091df12f075c75c84be61bf9f0b
Mateusz Solecki <solmat@gmail.com>
2021-01-04 12:18:50 +0100

    revert

--- src/Api.php
+++ src/Api.php
@@ -5,39 +5,41 @@ namespace Finelf_Api_Sdk;
 use function class_exists;
 use function ucfirst;
 
-class Api {
+class Api
+{
 	private static $instance;
 	private $client;
 
-	public function __construct( string $username, string $password, string $clientID, string $clientSecret, string $apiURL ) {
+	public static function getInstance(string $username, string $password, string $apiURL): Api
+	{
+		$can_connect = ! empty($username) && ! empty($password) && ! empty($apiURL);
+
+		if ($can_connect && self::$instance === null) {
+			self::$instance = new Api($username, $password, $apiURL);
+		}
+
+		return self::$instance;
+	}
+
+	public function __construct(string $username, string $password, string $apiURL)
+	{
 		$apiClient    = new ApiClient(
 			[
 				'username'     => $username,
 				'password'     => $password,
-				'clientID'     => $clientID,
-				'clientSecret' => $clientSecret,
 				'uri'          => $apiURL,
 			]
 		);
 		$this->client = $apiClient->createApiClient();
 	}
 
-	public static function getInstance( string $username, string $password, string $clientID, string $clientSecret, string $apiURL ): Api {
-		$can_connect = ! empty( $username ) && ! empty( $password ) && ! empty( $clientID ) && ! empty( $clientSecret ) && ! empty( $apiURL );
+	public function __get($name)
+	{
+		$class = 'Finelf_Api_Sdk\Modules\\'.ucfirst($name).'Module';
 
-		if ( $can_connect && self::$instance === null ) {
-			self::$instance = new Api( $username, $password, $clientID, $clientSecret, $apiURL );
-		}
-
-		return self::$instance;
-	}
-
-	public function __get( $name ) {
-		$class = 'Finelf_Api_Sdk\Modules\\' . ucfirst( $name ) . 'Module';
-
-		if ( class_exists( $class ) ) {
-			if ( ! isset( $this->$name ) ) {
-				$this->$name = new $class( $this->client );
+		if (class_exists($class)) {
+			if ( ! isset($this->$name)) {
+				$this->$name = new $class($this->client);
 			}
 
 			return $this->$name;
