f19fa61a7049fecc0e2fe31db0173d7e542f8ea4
Mateusz Solecki <solmat@gmail.com>
2021-01-04 12:19:10 +0100

    linter

--- src/Api.php
+++ src/Api.php
@@ -5,41 +5,37 @@ namespace Finelf_Api_Sdk;
 use function class_exists;
 use function ucfirst;
 
-class Api
-{
+class Api {
 	private static $instance;
 	private $client;
 
-	public static function getInstance(string $username, string $password, string $apiURL): Api
-	{
-		$can_connect = ! empty($username) && ! empty($password) && ! empty($apiURL);
-
-		if ($can_connect && self::$instance === null) {
-			self::$instance = new Api($username, $password, $apiURL);
-		}
-
-		return self::$instance;
-	}
-
-	public function __construct(string $username, string $password, string $apiURL)
-	{
+	public function __construct( string $username, string $password, string $apiURL ) {
 		$apiClient    = new ApiClient(
 			[
-				'username'     => $username,
-				'password'     => $password,
-				'uri'          => $apiURL,
+				'username' => $username,
+				'password' => $password,
+				'uri'      => $apiURL,
 			]
 		);
 		$this->client = $apiClient->createApiClient();
 	}
 
-	public function __get($name)
-	{
-		$class = 'Finelf_Api_Sdk\Modules\\'.ucfirst($name).'Module';
+	public static function getInstance( string $username, string $password, string $apiURL ): Api {
+		$can_connect = ! empty( $username ) && ! empty( $password ) && ! empty( $apiURL );
 
-		if (class_exists($class)) {
-			if ( ! isset($this->$name)) {
-				$this->$name = new $class($this->client);
+		if ( $can_connect && self::$instance === null ) {
+			self::$instance = new Api( $username, $password, $apiURL );
+		}
+
+		return self::$instance;
+	}
+
+	public function __get( $name ) {
+		$class = 'Finelf_Api_Sdk\Modules\\' . ucfirst( $name ) . 'Module';
+
+		if ( class_exists( $class ) ) {
+			if ( ! isset( $this->$name ) ) {
+				$this->$name = new $class( $this->client );
 			}
 
 			return $this->$name;
