da202db4e6d1231a0134d406fe14d1a74b5e50ab
Adrian <adrian.r@blueapps.pl>
2021-01-19 15:57:04 +0100

    implementation offers_clicks

--- src/DTO/OfferDTO.php
+++ src/DTO/OfferDTO.php
@@ -12,6 +12,7 @@ class OfferDTO extends BaseDTO {
     public $affiliateLink;
     public $prettyLink;
     public $displayName;
+    public $clicksCount;
     public $isEnabled;
     public $isActive;
     public $entity;
