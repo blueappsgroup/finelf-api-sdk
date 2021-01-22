31647891105a72287ee129025a87d4173d3baeda
Mateusz Solecki <solmat@gmail.com>
2021-01-22 11:51:40 +0100

    improvement

--- src/Modules/OfferModule.php
+++ src/Modules/OfferModule.php
@@ -5,26 +5,24 @@ namespace Finelf_Api_Sdk\Modules;
 use Finelf_Api_Sdk\DTO\OfferDTO;
 
 class OfferModule extends BaseModule {
-    protected $baseRoute = 'offers';
+	protected $baseRoute = 'offers';
 
-    public function getById(int $id, string $relations = ''): OfferDTO {
-        if (empty($id)) {
-            return new OfferDTO([]);
-        }
+	public function getById( int $id, string $relations = '' ): OfferDTO {
+		if ( empty( $id ) ) {
+			return new OfferDTO( [] );
+		}
 
-        if ( ! empty($relations)) {
-            $id .= '?relations='.$relations;
-        }
+		if ( ! empty( $relations ) ) {
+			$id .= '?relations=' . $relations;
+		}
 
-        return new OfferDTO(parent::get($id));
-    }
+		return new OfferDTO( parent::get( $id ) );
+	}
 
-    public function createClick(int $id): void {
-        if ( ! empty($id)) {
-            $id .= '/clicks';
-
-            parent::post($id);
-        }
-    }
+	public function createClick( int $id ): void {
+		if ( ! empty( $id ) ) {
+			parent::post( $id . '/clicks' );
+		}
+	}
 
 }
